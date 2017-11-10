f456f9e499de0f3affbf348aa5f4ad98eecb3899
Ashley Dattalo <adattalo@calpoly.edu>
2017-11-10 01:54:42 -0800

    super simple example of tf working

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,9 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void do_movement();
 
+void glfwSetUp();
+
+GLFWwindow* window;
 const GLuint WIDTH = 800, HEIGHT = 600;
 
 bool keys[1024]; 
@@ -31,69 +34,122 @@ GLfloat lastFrame = 0.0f;
 
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+const GLchar* vertexShaderSrc = 
+        "#version 330 core\n"
+        "in float inValue;\n"
+        "out float outValue;\n"
 
+        "void main()\n"
+        "{\n"
+            "outValue = sqrt(inValue);\n"
+        "}\n";
+
+//Shader *lightingShader = new Shader(SHADER_PATH "lighting.vert", SHADER_PATH "lighting.frag");
 int main()
 {
-    glfwInit();
+    glfwSetUp();
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    GLuint shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(shader, 1, &vertexShaderSrc, nullptr);
+    glCompileShader(shader);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "TransformFeedback", nullptr, nullptr);    
-    if (window == nullptr)
+    GLuint program = glCreateProgram();
+    glAttachShader(program, shader);
+
+    const GLchar* feedbackVaryings[] = { "outValue" };
+    glTransformFeedbackVaryings(program, 1, feedbackVaryings, GL_INTERLEAVED_ATTRIBS);
+
+    glLinkProgram(program);
+    glUseProgram(program);
+
+    // Create VAO
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    GLfloat data[] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f };
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+
+    GLint inputAttrib = glGetAttribLocation(program, "inValue");
+    glEnableVertexAttribArray(inputAttrib);
+    glVertexAttribPointer(inputAttrib, 1, GL_FLOAT, GL_FALSE, 0, 0);
+
+    GLuint tbo;
+    glGenBuffers(1, &tbo);
+    glBindBuffer(GL_ARRAY_BUFFER, tbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(data), nullptr, GL_STATIC_READ);
+
+
+    glBindBufferBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0, tbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+
+
+
+
+
+    int i = 0;
+    // while (!glfwWindowShouldClose(window))
+    while (i++ < 100)
     {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    
-    glewExperimental = GL_TRUE;
-
-    if (glewInit() != GLEW_OK)
-    {
-        std::cout << "Failed to initialize GLEW" << std::endl;
-        return -1;
-    }    
-
-    // Define the viewport dimensions
-    int width, height;
-    glfwGetFramebufferSize(window, &width, &height);  
-    glViewport(0, 0, width, height);
-    glEnable(GL_DEPTH_TEST);
-
-    //Shader *lightingShader = new Shader(SHADER_PATH "lighting.vert", SHADER_PATH "lighting.frag");
-
-    while (!glfwWindowShouldClose(window))
-    {
+        std::cout << "i : " << i << std::endl;
         // Calculate deltatime of current frame
-        GLfloat currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
+        // GLfloat currentFrame = glfwGetTime();
+        // deltaTime = currentFrame - lastFrame;
+        // lastFrame = currentFrame;
+        // //glfwSetWindowTitle(window, );
 
-        // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
-        glfwPollEvents();
-        do_movement();
+        // // Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
+        // glfwPollEvents();
+        // do_movement();
 
-        // Clear the colorbuffer
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // // Clear the colorbuffer
+        // glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        // glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        // Create camera transformations
-        glm::mat4 view;
-        view = camera.GetViewMatrix();
-        glm::mat4 projection = glm::perspective(camera.Zoom + 20.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 200.0f);
+        // // Create camera transformations
+        // glm::mat4 view;
+        // view = camera.GetViewMatrix();
+        // glm::mat4 projection = glm::perspective(camera.Zoom + 20.0f, (GLfloat)WIDTH / (GLfloat)HEIGHT, 0.1f, 200.0f);
 
-        // Swap the screen buffers
-        glfwSwapBuffers(window);
+
+        // Perform transform feedback
+        glEnable(GL_RASTERIZER_DISCARD);
+        glBeginTransformFeedback(GL_POINTS);
+            glDrawArrays(GL_POINTS, 0, 5);
+        glEndTransformFeedback();
+        glDisable(GL_RASTERIZER_DISCARD);
+        glFlush();
+
+
+        GLfloat feedback[5];
+        glGetBufferSubData(GL_TRANSFORM_FEEDBACK_BUFFER, 0, sizeof(feedback), feedback);
+
+        printf("%f %f %f %f %f\n", feedback[0], feedback[1], feedback[2], feedback[3], feedback[4]);
+
+        for (int i = 0; i < 5; i ++) {
+            data[i] = feedback[i];
+        }
+        printf("%f %f %f %f %f\n", data[0], data[1], data[2], data[3], data[4]);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, 5*sizeof(float), &data[0]);
+
+
+
+        // glfwSwapBuffers(window);
+
+
     }
+    glDeleteProgram(program);
+    glDeleteShader(shader);
+
+    glDeleteBuffers(1, &tbo);
+    glDeleteBuffers(1, &vbo);
+
+    glDeleteVertexArrays(1, &vao);
     return 0;
 }
 
@@ -139,4 +195,38 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos){
     lastY = ypos;
 
     camera.ProcessMouseMovement(xoffset, yoffset);
-}  
+}
+
+void glfwSetUp() {
+    glfwInit();
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "TransformFeedback", nullptr, nullptr);    
+    if (window == nullptr)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+    {
+        std::cout << "Failed to initialize GLEW" << std::endl;
+    }    
+
+    // Define the viewport dimensions
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);  
+    glViewport(0, 0, width, height);
+    glEnable(GL_DEPTH_TEST);
+}
